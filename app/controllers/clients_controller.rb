fd7de24a28c06773f531a803e5cd710db68a31fd
Amie Foster <amie.n.foster@gmail.com>
2021-11-07 23:25:09 -0700

    working on extra deliverable

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,13 +1,33 @@
 class ClientsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :client_not_found
 
+    def index
+        clients = Client.all
+        render json: clients
+    end
+
     def show
         client = Client.find(params[:id])
-        render json: client
+        render json: client, methods: :client_total
+    end
+
+    def update
+        client = Client.find(params[:id]).update!(client_params)
+        render json: client, status: :ok
     end
 
     private 
+
+    def client_params
+        params.permit(:name, :age)
+    end
+
     def client_not_found
         render json: { error: "Client not found" }, status: :not_found
+    end
+
+    def client_total
+        client.find(params[:id])
+        client.memberships.sum(:charge)
     end
 end
--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,9 +1,19 @@
 class GymsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :gym_not_found
 
+    def index
+        gym = Gym.all
+        render json: gym
+    end
+
     def show
         gym = Gym.find(params[:id])
         render json: gym, status: :ok
+    end
+
+    def update
+        gym = Gym.find(params[:id]).update!(gym_params)
+        render json: gym, status: :created
     end
 
     def destroy
@@ -14,8 +24,12 @@ class GymsController < ApplicationController
 
     private
 
+    def gym_params
+        params.permit(:name, :address)
+    end
+
     def gym_not_found
         render json: { error: "Gym not found" }, status: :not_found
     end
-    
+
 end
